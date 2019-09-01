4fced9df3314e42ef4684ff12e4526480293490f
sealema <54751372+sealema@users.noreply.github.com>
2019-09-01 15:58:51 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var express 	= require('express'),
 	app			= express(),
     server  	= require('http').createServer(app),
     io      	= require('socket.io').listen(server),
-    port    	= 8080,
+    port    	= 3000,
 
     // hash object to save clients data,
     // { socketid: { clientid, nickname }, socketid: { ... } }
